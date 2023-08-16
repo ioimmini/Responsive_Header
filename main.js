6d696d00333562c08fd07179361fd41bf1dbf6ab
ioimmini <jumin0114@gmail.com>
2023-08-16 13:52:16 +0900

    LAST

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-const toogleBtn = document.querySelector('.h_toogleBtn');
+const toggleBtn = document.querySelector('.h_toggleBtn');
 const menu =document.querySelector('.h_menu');
 const link =document.querySelector('.h_link');
 
-toogleBtn.addEventListener('click', () => {
-    menu.classList.toogle('active');
+toggleBtn.addEventListener('click', () => {
+    menu.classList.toggle('active');
     link.classList.toggle('active');
 });
